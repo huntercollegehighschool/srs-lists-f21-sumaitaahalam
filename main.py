83b180d5d0bfb37db756bbdd1de967e802474186
sumaitaahalam <75225835+sumaitaahalam@users.noreply.github.com>
2021-11-12 17:32:10 +0000

    tried to do 1 and 3:(

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Program1 import number_of_odds
 from Program2 import odd_reverse
 from Program3 import second_smallest
-from Program4 import greater_than_mean
+from Program4 import above_average
 
 program = input("Which program(1, 2, 3 or 4)? ")
 
@@ -19,4 +19,4 @@ elif program == '3':
 
 elif program == '4':
   lis = [int(i) for i in input("Enter a list of numbers separated by a space: ").split()]
-  print(greater_than_mean(lis))
+  print(above_average(lis))
